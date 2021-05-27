16f391baa2bfdfbd52274f913f938b7908308eb3
RenattoAlencar <alencar.renato@yahoo.com.br>
2021-05-27 16:59:52 -0300

    refact(page welcome: button)

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,37 +1,31 @@
-import React, { useState } from 'react'
-import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity } from 'react-native'
-import { Button } from '../components/Button'
+import React from 'react'
+import { Dimensions, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity } from 'react-native'
 
 import watering from '../assets/watering.png'
 
 import colors from '../styles/colors'
 
 export function Welcome() {
-  const [visible, setVisible] = useState(false)
-
-  function handleVisible() {
-    setVisible(true)
-  }
-
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>
         Gerencie {'\n'} suas plantas {'\n'} de forma fácil
       </Text>
 
-
-      <Image source={watering} style={styles.image} />
+      <Image source={watering} style={styles.image} resizeMode='contain' />
 
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas.
         Nós cuidamos de lembrar você sempre que precisar.
       </Text>
 
-      <Button title='>' />
+      <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+        <Text style={styles.buttonText}>
+          {'>'}
+        </Text>
+      </TouchableOpacity >
 
-
-
-    </SafeAreaView>
+    </SafeAreaView >
   )
 }
 
@@ -39,7 +33,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around'
   },
   title: {
     fontSize: 32,
@@ -55,8 +49,20 @@ const styles = StyleSheet.create({
     color: colors.heading
   },
   image: {
-    width: 292,
-    height: 284
+    height: Dimensions.get('window').width * 0.7
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24
   }
 
 })
