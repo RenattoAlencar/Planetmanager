d540c2bb4b2cc492abd01a860931f75e6df2f7e2
RenattoAlencar <alencar.renato@yahoo.com.br>
2021-05-29 18:14:49 -0300

    feat(useEffect api fake )

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 import { Header } from '../components/Header'
 import { EnviromentButton } from '../components/EnviromentButton'
+import { FlatList } from 'react-native-gesture-handler'
 
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
-import { FlatList } from 'react-native-gesture-handler'
+
+import api from '../services/api'
+
+interface EnviromentsProps {
+  key: string,
+  title: string
+}
 
 export function PlantSelect() {
+  const [enviroments, setEnviroments] = useState<EnviromentsProps[]>([])
+
+  useEffect(() => {
+    async function fetchEnviroment() {
+      const { data } = await api.get('plants_environments')
+      setEnviroments([
+        {
+          key: 'all',
+          title: 'Todos'
+        },
+        ...data
+      ])
+    }
+    fetchEnviroment()
+  }, [])
+
   return (
     <View style={styles.container} >
       <View style={styles.header} >
@@ -17,8 +40,8 @@ export function PlantSelect() {
         <Text style={styles.subtitle} >você quer colocar sua planta?</Text>
       </View>
       <View>
-        <FlatList data={[1, 2, 3, 4, 5, 6, 7, 8]} renderItem={({ item }) => {
-          return <EnviromentButton title='Cozinha' active /> //COLOCA KEY ID
+        <FlatList data={enviroments} renderItem={({ item }) => {
+          return <EnviromentButton key={item.key} title={item.title} />
 
         }} horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.enviromentList} />
       </View>
