f5713c2786e116251d6508b0ba1c68992e3ce0b4
RenattoAlencar <alencar.renato@yahoo.com.br>
2021-05-29 15:57:32 -0300

    feat(pages:Header, component:ButtonList)

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
+import { Header } from '../components/Header'
+import { EnviromentButton } from '../components/EnviromentButton'
+
 import colors from '../styles/colors'
+import fonts from '../styles/fonts'
+import { FlatList } from 'react-native-gesture-handler'
 
 export function PlantSelect() {
   return (
     <View style={styles.container} >
-      <Text>
-        Seleciona Planta
-      </Text>
+      <View style={styles.header} >
+        <Header />
+        <Text style={styles.title} >Em qual ambiente</Text>
+        <Text style={styles.subtitle} >você quer colocar sua planta?</Text>
+      </View>
+      <View>
+        <FlatList data={[1, 2, 3, 4, 5, 6, 7, 8]} renderItem={({ item }) => {
+          return <EnviromentButton title='Cozinha' active />
+
+        }} horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.enviromentList} />
+      </View>
     </View>
   )
 }
@@ -16,6 +29,29 @@ export function PlantSelect() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.background
+    backgroundColor: colors.background,
+  },
+  header: {
+    paddingHorizontal: 30
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15
+  },
+  subtitle: {
+    fontSize: 17,
+    fontFamily: fonts.text,
+    lineHeight: 20,
+    color: colors.heading
+  },
+  enviromentList: {
+    height: 40,
+    justifyContent: 'center',
+    paddingBottom: 5,
+    marginLeft: 32,
+    marginVertical: 32
   }
 })
